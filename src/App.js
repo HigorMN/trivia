4ce643e21c8bfa10a2899382117388c2b1391498
HigorMN <higor.maranhao2000@gmail.com>
2022-10-07 18:45:07 -0300

    estrutura req 05

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Trivia from './pages/Trivia';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+
+      <Route exact path="/trivia" component={ Trivia } />
     </Switch>
   );
 }
