59efbe15b0df39fd1ef2bd9ed91f62ebe7db8c11
HigorMN <higor.maranhao2000@gmail.com>
2022-10-07 19:03:39 -0300

    reparando

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+
 import Trivia from './pages/Trivia';
 
 export default function App() {
