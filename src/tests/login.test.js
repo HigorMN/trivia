d7a353421c07f8d27cb04eb25a1ff1bca063277d
HigorMN <higor.maranhao2000@gmail.com>
2022-10-11 17:45:33 -0300

    Requisito 04 - ok

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,46 +1,39 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux'
-import Login from '../pages/Login';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import App from '../App';
 
 
 describe("<Login />", () => {
-    test('renderizar o campo de email', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByTestId("input-gravatar-email");
-        expect(inputELement).toBeInTheDocument();
-        expect(inputELement).toHaveAttribute("type", "email");
-    });
-    test('testar se o email é válido', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByTestId("input-gravatar-email")
-        userEvent.type(inputELement, "test@email.com");
-        expect(screen.getByTestId("input-gravatar-email")).toHaveValue("test@email.com")
-        expect(screen.queryByTestId("error-msg")).not.toBeInTheDocument();
-    });
-    test('renderizar o campo de playerName', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByTestId("input-player-name");
-        expect(inputELement).toBeInTheDocument();
-        expect(inputELement).toHaveAttribute("type", "text");
-    });
-    test('testar se o playerName é válido', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByTestId("input-player-name")
-        userEvent.type(inputELement, "playerName");
-        expect(screen.getByTestId("input-player-name")).toHaveValue("playerName")
-        expect(screen.queryByTestId("error-msg")).not.toBeInTheDocument();
-    });
-    test('renderizar o botão', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByTestId("btn-play");
-        expect(inputELement).toBeInTheDocument();
-        expect(inputELement).toHaveAttribute("type", "submit");
+    test('Testando se ao digitar email e nome e clicar em jogar é redirecionado ao jogo', async () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+
+        const inputEmail = screen.getByTestId("input-gravatar-email");
+        expect(inputEmail).toBeInTheDocument();
+
+        userEvent.type(inputEmail, "test@email.com");
+        const btnJogar = screen.getByRole('button', {  name: /jogar/i});
+        expect(btnJogar).toBeInTheDocument();
+        expect(btnJogar.disabled).toBe(true);
+
+        const inputName = screen.getByTestId("input-player-name");
+        userEvent.type(inputName, "playerName");
+        expect(inputName).toBeInTheDocument();
+        expect(btnJogar.disabled).toBe(false);
+
+        userEvent.click(btnJogar);
+
+        const playrName = await screen.findByText(/playername/i);
+        expect(playrName).toBeInTheDocument();
+        expect(history.location.pathname).toBe('/trivia');
     });
 
     test('renderizar o botão de configuração', () => {
-        renderWithRouterAndRedux(<Login />);
-        const inputELement = screen.getByRole('button', {  name: /configuração/i});
+        const { history } = renderWithRouterAndRedux(<App />);
+        const inputELement = screen.getByRole('button', { name: /configuração/i});
         expect(inputELement).toBeInTheDocument();
+
+        userEvent.click(inputELement);
+        expect(history.location.pathname).toBe('/config')
     });
 });
