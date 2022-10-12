ddbccbc87f346e79dec3b9e11eedcc3a1da2a08e
HigorMN <higor.maranhao2000@gmail.com>
2022-10-12 18:47:34 -0300

    css ranking ok

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -5,6 +5,9 @@ import md5 from 'crypto-js/md5';
 import restScore from '../redux/action';
 import { getPlayrsLocal } from '../services/saveLocal';
 
+import logoTrivia from '../images/logoTrivia.png';
+import StarImage from '../components/StarImage';
+
 class Ranking extends Component {
   goHome = () => {
     const { history, dispatch } = this.props;
@@ -16,26 +19,40 @@ class Ranking extends Component {
     const playrs = getPlayrsLocal() || [];
     playrs.sort((a, b) => b.score - a.score);
     return (
-      <main>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.goHome }
-        >
-          Ir para Home
-        </button>
-        <div>
+      <main className="ranking-main-container center">
+        <img src={ logoTrivia } alt="Logo trivia" className="ranking-logo-trivia" />
+        <div className="ranking-container center">
+          <h1 data-testid="ranking-title" className="ranking-title">Ranking</h1>
           {playrs.map((e, index) => (
-            <div key={ index }>
-              <img
-                src={ `https://www.gravatar.com/avatar/${md5(e.email).toString()}` }
-                alt="Imgaem Gravatar"
-              />
-              <p data-testid={ `player-name-${index}` }>{e.playerName}</p>
-              <p data-testid={ `player-score-${index}` }>{e.score}</p>
+            <div key={ index } className="ranking-container-r center">
+              <div className="center">
+                <img
+                  src={ `https://www.gravatar.com/avatar/${md5(e.email).toString()}` }
+                  alt="Imgaem Gravatar"
+                  className="ranking-gravatar"
+                />
+                <p data-testid={ `player-name-${index}` }>{e.playerName}</p>
+              </div>
+              <div className="ranking-star center">
+                <StarImage />
+                <p
+                  data-testid={ `player-score-${index}` }
+                  className="mg1"
+                >
+                  {e.score}
+                </p>
+                <p>pontos</p>
+              </div>
             </div>
           ))}
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.goHome }
+            className="btn-play-again bn"
+          >
+            JOGAR NOVAMENTE
+          </button>
         </div>
       </main>
     );
