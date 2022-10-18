bc7e07a1136bf49a34044e5eefba9da9c9de20c6
HigorMN <higor.maranhao2000@gmail.com>
2022-10-17 22:48:40 -0300

    Removendo mock desnecessario

--- src/tests/helpers/mockRanking.js
+++
@@ -1,9 +0,0 @@
-const playrsMock = [
-  {"score":162,"email":"vkg.marcos@gmail.com","playerName":"Marcos Vinicios"},
-  {"score":166,"email":"fe968000322@gmail.com","playerName":"Felipe Bresciani"},
-  {"score":167,"email":"arthurr.sydney@gmail.com","playerName":"Arthur Sydney"},
-  {"score":105,"email":"marcoscardoso825@gmail.com","playerName":"Marcos Cardoso"},
-  {"score":20,"email":"higor.maranhao2000@gmail.com","playerName":"Higor"},
-]
-
-export default playrsMock;
